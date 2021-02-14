8144e987f3e062a0c9d7cfa022d387f8c1f5a31b
kimikurata <MariKurata@MacBook-Pro-de-Mari.local>
2021-02-14 02:32:17 -0800

    recipe navbar

---
+++ kurata.kimi/recipes/js/javascript.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	// reference code from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_active_element
+			var link = $(".link_item")
+			// for(var i= 0; i < link.length; i++){
+			// 	link[i].addEventListener("click", function(){
+			// 		var current = $(".active");
+			// 		current[0].className = current[0].className.replace(" active", "");
+		 //  			this.className += " active";
+			// 	});
+			// };
+	//change 
+
+	// reference code from https://stackoverflow.com/questions/9979827/change-active-menu-item-on-page-scroll
+	var topMenu = $("#top-menu");
+	var    topMenuHeight = topMenu.outerHeight()+15;
+    // All list items
+    var menuItems = topMenu.find("a");
+    console.log(menuItems);
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      
+      if (item.length) { 
+      	return item; 
+      }
+    });
+    console.log( scrollItems);
+
+
+
+    
+
+    $(window).scroll(function(){
+   		// Get container scroll position
+  		var fromTop = $(this).scrollTop()+topMenuHeight;
+   	//	console.log(fromTop);
+
+   		// Get id of current scroll item
+   		var cur = scrollItems.map(function(){
+     	if ($(this).offset().top < fromTop)
+       		return this;
+   		});
+   		 // Get the id of the current element
+   		cur = cur[cur.length-1];
+   			var id = cur && cur.length ? cur[0].id : "";
+   			 menuItems
+     .parent().removeClass("active")
+     .end().filter("[href='#"+id+"']").parent().addClass("active");
+   });
+ 
+
+});
